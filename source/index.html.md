9445d9c8b784c29d9a084d179cdd8b1c79055672
Lajule <julien.rouzieres@mac.com>
2022-04-06 23:10:57 +0200

    Intermediate commit

--- source/index.html.md
+++ source/index.html.md
@@ -3,6 +3,7 @@ tickets:
   - TIC-001
 ---
 # Sprint name goes here
+Velocity: 30, From: 20220101, To: 20220131
 
 Add sprint description here.
 
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,10 +3,42 @@
 ;(function () {
     'use strict';
 
-    $(hello);
+    $(preview);
   
-    function hello() {
-      console.log("Hello !");
+    function preview() {
+      $('.ticket').each(function () {
+        var ticket = $(this);
+        var num = ticket.attr('data-ticket');
+        var title = ticket.find('h1').text();
+
+        var sp = 0;
+        ticket.find('p').each(function () {
+          var re = /Story points: (\d+)/i
+          var text = $(this).text();
+          var groups = re.exec(text);
+
+          if (groups) {
+            sp = groups[1]
+          }
+        });
+
+        var assignee = ''
+        ticket.find('p').each(function () {
+          var re = /Assignee: ([A-Za-z0-9_]+)/i
+          var text = $(this).text();
+          var groups = re.exec(text);
+
+          if (groups) {
+            assignee = groups[1]
+          }
+        });
+
+        console.log(num);
+        console.log(title);
+        console.log(sp);
+        console.log(assignee);
+
+      });
     }
   })();
   
