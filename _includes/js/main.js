a3bad5c96591884fefa9de05a82533b32e890f6d
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2022-04-08 17:37:38 +0200

    Add chart

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,5 +1,29 @@
 window.addEventListener('DOMContentLoaded', function (event) {
+    const sprint = document.querySelector('.sprint');
 
-    console.log("Hello");
+    const days = [];
+    const to = new Date(sprint.dataset.to);
+    for (const from = new Date(sprint.dataset.from); from <= to; from.setDate(from.getDate() + 1)) {
+	days.push(new Date(from.getTime()));
+    }
 
+    const tickets = Array.from(sprint.querySelectorAll('.sprint-ticket')).map(ticket => ticket.dataset);
+
+    const canvas = document.getElementById('sprint-brundown-chart');
+    new Chart(canvas.getContext('2d'), {
+	type: 'line',
+	data: {
+            labels: days,
+            datasets: [{
+		data: [12, 19, 3, 5, 2, 3],
+            }]
+	},
+	options: {
+            scales: {
+		xAxes: [{
+		    type: 'time'
+		}]
+            }
+	}
+    });
 });
--- index.md
+++ index.md
@@ -1,7 +1,7 @@
 ---
 layout: default
 title: "Happy Jekylling!"
-from: 2022-12-17
+from: 2022-12-01
 to: 2022-12-17
 ---
 
