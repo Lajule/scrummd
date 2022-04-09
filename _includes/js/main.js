bedfcc80eb960bf87e696ec1642773d90b3bd238
Lajule <julien.rouzieres@mac.com>
2022-04-09 23:43:23 +0200

    Better Js variables names

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,60 +1,42 @@
 window.addEventListener('DOMContentLoaded', function (event) {
     const sprint = document.querySelector('.sprint');
     const tickets = Array.from(sprint.querySelectorAll('.sprint-ticket')).map(ticket => ticket.dataset);
+    const canvas = document.getElementById('sprint-brundown-chart');
 
-    const sprintDays = [];
+    const daysInSprint = [];
     const to = new Date(sprint.dataset.to);
     for (const from = new Date(sprint.dataset.from); from <= to; from.setDate(from.getDate() + 1)) {
-        sprintDays.push(from.toLocaleDateString());
+        daysInSprint.push(from.toLocaleDateString());
     }
 
     const sprintComplexity = parseInt(sprint.dataset.complexity);
 
-    const dataIdeal = [{x: sprintDays[0], y: sprintComplexity}, {x: sprintDays[sprintDays.length - 1], y: 0}];
+    const dataIdeal = [{x: daysInSprint[0], y: sprintComplexity}, {x: daysInSprint[daysInSprint.length - 1], y: 0}];
 
-    const completedMap = tickets.reduce((acc, curr) => {
+    const doneTicketsMap = tickets.reduce((acc, curr) => {
         if (curr.status == 'DONE') {
             const updated = new Date(curr.updated).toLocaleDateString();
-            if (!acc[updated]) {
-                acc[updated] = 0;
-            }
+            if (!acc[updated]) { acc[updated] = 0; }
             acc[updated] += parseInt(curr.complexity);
         }
         return acc;
     }, {})
 
-    const dataCompleted = [];
+    const doneTicketsData = [];
     let currentComplexity = sprintComplexity;
-    for (let i = 0; i < sprintDays.length; ++i) {
-        const day = sprintDays[i];
-        if (completedMap[day]) {
-            currentComplexity -= completedMap[day];
-        }
-        dataCompleted.push({
-            x: sprintDays[i],
-            y: currentComplexity
-        });
+    for (let i = 0; i < daysInSprint.length; ++i) {
+        const day = daysInSprint[i];
+        if (doneTicketsMap[day]) { currentComplexity -= doneTicketsMap[day]; }
+        doneTicketsData.push({x: daysInSprint[i], y: currentComplexity});
     }
 
-    const canvas = document.getElementById('sprint-brundown-chart');
     new Chart(canvas.getContext('2d'), {
         type: 'line',
         data: {
-            labels: sprintDays,
+            labels: daysInSprint,
             datasets: [
-                {
-                    label: "Ideal",
-		            data: dataIdeal,
-                    borderColor: 'rgb(75, 192, 192)',
-                    backgroundColor: 'rgb(75, 192, 192)'
-                },
-                {
-                    label: "Completed",
-		            data: dataCompleted,
-                    stepped: true,
-                    borderColor: 'rgb(255, 159, 64)',
-                    backgroundColor: 'rgb(255, 159, 64)'
-                },
+                {label: "Ideal", data: dataIdeal, borderColor: 'rgb(75, 192, 192)', backgroundColor: 'rgb(75, 192, 192)'},
+                {label: "Completed", data: doneTicketsData, stepped: true, borderColor: 'rgb(255, 159, 64)', backgroundColor: 'rgb(255, 159, 64)'},
             ]
 	    }
     });
