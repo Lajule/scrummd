e0b60efd5f10a63fb2150b50360709719882b54d
Lajule <julien.rouzieres@mac.com>
2022-04-09 21:29:17 +0200

    Brundown + ticket pages

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,27 +1,61 @@
 window.addEventListener('DOMContentLoaded', function (event) {
     const sprint = document.querySelector('.sprint');
+    const tickets = Array.from(sprint.querySelectorAll('.sprint-ticket')).map(ticket => ticket.dataset);
 
-    const days = [];
+    const sprintDays = [];
     const to = new Date(sprint.dataset.to);
     for (const from = new Date(sprint.dataset.from); from <= to; from.setDate(from.getDate() + 1)) {
-        days.push(new Date(from.getTime()));
+        sprintDays.push(from.toLocaleDateString());
     }
 
-    const tickets = Array.from(sprint.querySelectorAll('.sprint-ticket')).map(ticket => ticket.dataset);
     const totalComplexity = tickets.reduce((acc, curr) => acc + parseInt(curr.complexity), 0);
 
-    console.log(days[0]);
-    console.log(days[days.length - 1]);
+    const dataIdeal = [{x: sprintDays[0], y: totalComplexity}, {x: sprintDays[sprintDays.length - 1], y: 0}];
 
+    const completedMap = tickets.reduce((acc, curr) => {
+        if (curr.status == 'DONE') {
+            const updated = new Date(curr.updated).toLocaleDateString();
+            if (!acc[updated]) {
+                acc[updated] = 0;
+            }
+            acc[updated] += parseInt(curr.complexity);
+        }
+        return acc;
+    }, {})
+
+    const dataCompleted = [];
+    let currentComplexity = totalComplexity;
+    for (let i = 0; i < sprintDays.length; ++i) {
+        const day = sprintDays[i];
+        if (completedMap[day]) {
+            currentComplexity -= completedMap[day];
+        }
+        dataCompleted.push({
+            x: sprintDays[i],
+            y: currentComplexity
+        });
+    }
 
     const canvas = document.getElementById('sprint-brundown-chart');
     new Chart(canvas.getContext('2d'), {
         type: 'line',
         data: {
-            labels: days,
-            datasets: [{
-		        data: [{x: days[0], y: totalComplexity}, {x: days[days.length - 1], y: 0}],
-            }]
+            labels: sprintDays,
+            datasets: [
+                {
+                    label: "Ideal",
+		            data: dataIdeal,
+                    borderColor: 'rgb(75, 192, 192)',
+                    backgroundColor: 'rgb(75, 192, 192)'
+                },
+                {
+                    label: "Completed",
+		            data: dataCompleted,
+                    stepped: true,
+                    borderColor: 'rgb(255, 159, 64)',
+                    backgroundColor: 'rgb(255, 159, 64)'
+                },
+            ]
 	    }
     });
 });
--- index.md
+++ index.md
@@ -2,7 +2,7 @@
 layout: default
 title: "Happy Jekylling!"
 from: 2022-12-01
-to: 2022-12-17
+to: 2022-12-15
 ---
 
 Add sprint description here.
