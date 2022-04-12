d5a408e757d4010cafa9d4efb48796ea6efa91cc
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2022-04-12 15:48:42 +0200

    Change chart background

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,68 +1,93 @@
 window.addEventListener('DOMContentLoaded', function(event) {
-	const sprint = document.querySelector('.sprint');
-	const tickets = sprint.querySelectorAll('.sprint-ticket');
-	const canvas = document.getElementById('sprint-brundown-chart');
+    const sprint = document.querySelector('.sprint');
+    const tickets = sprint.querySelectorAll('.sprint-ticket');
+    const canvas = document.getElementById('sprint-brundown-chart');
 
-	let complexity = parseInt(sprint.dataset.complexity, 10);
+    let complexity = parseInt(sprint.dataset.complexity, 10);
 
-	const doneTicketsMap = Array.from(tickets).reduce((acc, curr) => {
-		if (curr.dataset.status == 'DONE') {
-			const updated = new Date(curr.dataset.updated).toLocaleDateString();
-			if (!acc[updated]) {
-				acc[updated] = 0;
-			}
-			acc[updated] += parseInt(curr.dataset.complexity, 10);
-		}
-		return acc;
-	}, {})
+    const doneTicketsMap = Array.from(tickets).reduce((acc, curr) => {
+        if (curr.dataset.status == 'DONE') {
+            const updated = new Date(curr.dataset.updated).toLocaleDateString();
+            if (!acc[updated]) {
+                acc[updated] = 0;
+            }
+            acc[updated] += parseInt(curr.dataset.complexity, 10);
+        }
+        return acc;
+    }, {})
 
-	const chartData = {
-		labels: [],
-		datasets: [{
-				label: "Ideal",
-				data: [],
-				borderColor: 'rgb(51, 51, 51)',
-				backgroundColor: 'rgb(51, 51, 51)'
-			},
-			{
-				label: "Completed",
-				data: [],
-				stepped: true,
-				borderColor: 'rgb(255, 159, 64)',
-				backgroundColor: 'rgb(255, 159, 64)'
-			}
-		]
-	};
+    const chartData = {
+        labels: [],
+        datasets: [{
+                label: "Ideal",
+                data: [],
+                borderColor: 'rgb(51, 51, 51)',
+                backgroundColor: 'rgb(51, 51, 51)'
+            },
+            {
+                label: "Completed",
+                data: [],
+                stepped: true,
+                borderColor: 'rgb(255, 159, 64)',
+                backgroundColor: 'rgb(255, 159, 64)'
+            }
+        ]
+    };
 
-	const to = new Date(sprint.dataset.to);
-	for (const from = new Date(sprint.dataset.from); from <= to; from.setDate(from.getDate() + 1)) {
-		chartData.labels.push(from.toLocaleDateString());
-	}
+    const to = new Date(sprint.dataset.to);
+    for (const from = new Date(sprint.dataset.from); from <= to; from.setDate(from.getDate() + 1)) {
+        chartData.labels.push(from.toLocaleDateString());
+    }
 
-	chartData.datasets[0].data = [{
-		x: chartData.labels[0],
-		y: complexity
-	}, {
-		x: chartData.labels[chartData.labels.length - 1],
-		y: 0
-	}];
+    chartData.datasets[0].data = [{
+        x: chartData.labels[0],
+        y: complexity
+    }, {
+        x: chartData.labels[chartData.labels.length - 1],
+        y: 0
+    }];
 
-	for (let i = 0; i < chartData.labels.length; ++i) {
-		const day = chartData.labels[i];
-		if (doneTicketsMap[day]) {
-			complexity -= doneTicketsMap[day];
-		}
-		chartData.datasets[1].data.push({
-			x: chartData.labels[i],
-			y: complexity
-		});
-	}
+    for (let i = 0; i < chartData.labels.length; ++i) {
+        const day = chartData.labels[i];
+        if (doneTicketsMap[day]) {
+            complexity -= doneTicketsMap[day];
+        }
+        chartData.datasets[1].data.push({
+            x: chartData.labels[i],
+            y: complexity
+        });
+    }
 
-	new Chart(canvas.getContext('2d'), {
-		type: 'line',
-		data: chartData,
-		options: {
-		    responsive: true
-		}
-	});
+    new Chart(canvas.getContext('2d'), {
+        type: 'line',
+        data: chartData,
+        options: {
+            responsive: true
+        },
+        plugins: [{
+            id: 'custom-canvas-background-color',
+            beforeDraw: (chart, args, options) => {
+                const {
+                    ctx,
+                    chartArea: {
+                        top,
+                        right,
+                        bottom,
+                        left,
+                        width,
+                        height
+                    },
+                    scales: {
+                        x,
+                        y
+                    },
+                } = chart;
+                ctx.save();
+                ctx.globalCompositeOperation = 'destination-over';
+                ctx.fillStyle = 'rgb(253, 253, 253)';
+                ctx.fillRect(left, top, width, height);
+                ctx.restore();
+            },
+        }]
+    });
 });
