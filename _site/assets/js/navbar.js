741d32c1b1542c06998f9f9ae8aa012bd6ff86ec
Lajule <julien.rouzieres@mac.com>
2022-04-29 21:52:32 +0200

    Add brand

--- _site/assets/js/navbar.js
+++ _site/assets/js/navbar.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    const burgers = Array.from(document.querySelectorAll('.navbar-burger'));
+document.addEventListener('DOMContentLoaded', function (event) {
+	const burgers = Array.from(document.querySelectorAll('.navbar-burger'));
 
-    burgers.forEach(burger => {
-	burger.addEventListener('click', () => {
-	    const target = document.getElementById(burger.dataset.target);
+	burgers.forEach(burger => {
+		burger.addEventListener('click', () => {
+			const target = document.getElementById(burger.dataset.target);
 
-	    burger.classList.toggle('is-active');
-	    target.classList.toggle('is-active');
+			burger.classList.toggle('is-active');
+			target.classList.toggle('is-active');
+		});
 	});
-    });
 });
