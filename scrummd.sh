cdbdf94c962c838560e6bae8b954eaf62641990d
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2022-05-05 17:15:08 +0200

    CLI

--- scrummd.sh
+++ scrummd.sh
@@ -3,5 +3,17 @@
 dir="${0%/*}"
 base="${0##*/}"
 
+help_message="\
+Usage: $base [<options>] <command> [<command-options>]
+Run commands related to the scrummd process.
+Commands:
+  serve                   Run the jekyll server process, useful for
+                          development.
+  build                   Run the build process.
+Global Options:
+  -h, --help              Show this help information.
+  -v, --verbose           Increase verbosity. Useful for debugging.
+"
+
 echo "${dir}"
 echo "${base}"
